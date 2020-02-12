5166f268aac7088157bbe79320f8247e3aee63fa
Marcos Yuri Rosa Junior <marcosyuri.fsz@gmail.com>
2020-02-12 14:21:15 -0300

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => 
+<Main icon="home" title="Início" subtitle="Segundo Projeto">
+    <div className='display-4>'>Bem vindo</div>
+    <hr/>
+    <p clasName="mb-8">Sistema para exemplificar a construção e um cadastro desenvolvido em React</p>
+</Main>
---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+        <strong> Marcos<span className="text-danger">Yuri</span>JR</strong>
+    </span>
+</footer>
